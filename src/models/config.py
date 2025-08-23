c9e5874f8cc26f0bfa55eea07c67be3fe497763b
IdoXpoz <ido@hossen.co>
2025-08-23 10:27:16 +0300

    try to run new configuration with gemma

--- src/models/config.py
+++ src/models/config.py
@@ -1,5 +1,5 @@
 # ALL_OPEN_SOURCE_MODELS = ["google/gemma-3-4b-it", "meta-llama/Llama-3.2-3B-Instruct"]
-OPEN_SOURCE_MODELS = ["meta-llama/Llama-3.2-3B-Instruct"]
+OPEN_SOURCE_MODELS = ["google/gemma-3-4b-it"]
 # ALL_REASONING_MODELS = ["Qwen/Qwen3-4B"]
 REASONING_MODELS = []
 
