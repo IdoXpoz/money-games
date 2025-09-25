02f72454d782cac995c56dd03e30a3bd8d4c0b8a
IdoXpoz <ido@hossen.co>
2025-09-25 15:53:13 -0400

    llama graph new colors

--- src/analysis/trends_analysis.py
+++ src/analysis/trends_analysis.py
@@ -10,7 +10,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 from src.prompts.configs.money import PREFIXES
-from src.prompts.configs.games2 import DECISION_KEYWORDS
+from src.prompts.configs.games import DECISION_KEYWORDS
 
 MODEL_TO_CSV_PATH_MAP_GAME1 = {
     # "gemma-3-4b-it": "src/analysis/game1/gemma-3-4b-it_results.csv",
@@ -30,8 +30,8 @@ MODEL_TO_CSV_PATH_MAP_GAME2 = {
     # "chat-gemma-3-12b-pt": "src/analysis/game2/chat-gemma-3-12b-pt_results.csv",
     # "chat-gemma-3-12b-it": "src/analysis/game2/chat-gemma-3-12b-it_results.csv",
     # "chat-llama-3.2-3B-Instruct": "src/analysis/game2/chat-llama-3.2-3b-instruct_results.csv",
-    "Qwen-4b": "src/analysis/game2/qwen-4b_results_with_temperature.csv",
-    "Qwen-8b": "src/analysis/game2/qwen-8b_results_with_temperature.csv",
+    # "Qwen-4b": "src/analysis/game2/qwen-4b_results_with_temperature.csv",
+    # "Qwen-8b": "src/analysis/game2/qwen-8b_results_with_temperature.csv",
 }
 
 PXS = PREFIXES.keys()
@@ -99,7 +99,7 @@ def analyze_all():
     """
     Analyze results for both game1 and game2.
     """
-    for game in ["game2"]:
+    for game in ["game1"]:
         print(f"Analyzing {game}...")
         model_map, output_dir = get_model_map_and_output_dir(game)
         models_to_analyze = model_map.keys()
